dca40b4c9b7e15d75fa04925c5b7e0f52d7c8517
Tibor <bratoblog@gmail.com>
2023-05-11 14:22:04 +0200

    rework the functionality

--- javascript.js
+++ javascript.js
@@ -1,33 +1,36 @@
-const result = ["Rock", "Paper", "Sciccors"]
+const computerChoice = () => {
+  const choices = ['rock', 'paper', 'scissors'];
+  const index = Math.floor(Math.random() * choices.length);
+  return choices[index];
+};
 
-function getComputerChoice(){
-    let choice = Math.floor(Math.random() * (result.length));
-    return choice
-}
-getComputerChoice();
+const validator = () => {
+  const userChoice = document.querySelector("#user-input").value.toLowerCase();
+  const userInput = ['rock', 'paper', 'scissors'];
+  if (!userInput.includes(userChoice)) {
+    alert("You must enter a valid choice");
+  } else {
+    game(userChoice);
+  }
+};
 
+const game = (userChoice) => {
+  const computer = computerChoice();
+  let result;
+  if(userChoice === computer) {
+    console.log(userChoice);
+    console.log(computer);
+    result = "It's a tie!"
+  } else if(userChoice === "rock" && computer === "scissors" || userChoice === "paper" && computer === "rock" || userChoice === "scissors" && computer === "paper") {
+    console.log(userChoice);
+    console.log(computer);
+    result = "User wins!"
+  } else {
+    console.log(userChoice);
+    console.log(computer);
+    result = "Computer wins!"
+  }
+  document.querySelector("#result").innerHTML = result;
+};
 
-
-function playRound(playerSelection, computerSelection){
-if (playerSelection == computerSelection){
-    return "It's a tie!
-}
-else if (playerSelection == "Rock" && computerSelection == "Scissors"){
-  return "Player wins!"
-} else if (playerSelection == "Paper" && computerSelection == "Rock"){
-  return "Player wins!"
-} else if (playerSelection == "Scissors" && computerSelection == "Paper"){
-  return "Player wins!"
-} else if (playerSelection == "Scissors" && computerSelection == "Rock"){
-  return "Computer wins!"
-  } else if (playerSelection == "Rock" && computerSelection == "Paper"){
-  return "Computer wins!"
-   } else (playerSelection == "Paper" && computerSelection == "Scissors"){
-  return "Computer wins!"}
-}
-
-  const playerSelection = "Rock";
-  const computerSelection = getComputerChoice();
-  console.log(playerSelection);
-  console.log(computerSelection);
-  console.log(playRound(playerSelection, computerSelection))
+document.querySelector("#play-button").addEventListener("click", validator);
